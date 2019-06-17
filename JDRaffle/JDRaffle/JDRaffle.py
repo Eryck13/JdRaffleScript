898fcf2b252629ca2e26bd699547bb4761b07850
Eryck13 <eryckmarleau@hotmail.com>
2019-06-16 23:37:09 -0400

    Added some stuff.

--- JDRaffle/JDRaffle/JDRaffle.py
+++ JDRaffle/JDRaffle/JDRaffle.py
@@ -4,8 +4,6 @@ from time import sleep
 import random 
 import datetime
 import string 
-import pyarmor
-import pyfiglet
 from fake_useragent import UserAgent
 
 def get_proxy():
@@ -32,16 +30,19 @@ def get_proxy():
         proxy = random.choice(proxy_list)
         return proxy 
     except:
-        pass
+        proxies = open('proxies.txt', 'r').read().splitlines()
+        proxy = random.choice(proxies)
+        return proxy 
+
   
 def main():#'✓'
     
     
     url = 'https://reporting.jdsports.co.uk/cgi-bin/msite?yeezy_comp%2Ba%2B0%2B0%2B0%2B7831C9FC%2B0%26_p%3D_2%26utm_source%3DRedEye%26utm_medium%3DEmail%26utm_campaign%3DYeezy%2BBoost%2B350%2BClay%26utm_content%3D0805%2BYeezy%2BClay'
     
-    welcome = pyfiglet.figlet_format('Welcome to JDsports Raffle Script!')
-    print(welcome)
+    print("Welcome to JDSports Raffle Script!")
     
+    catchall = input('input your domain with @')
     town = input('Please enter your city with a capital letter. ')
     county= input('Please enter the county you live in with a capital letter. ')
     postcode = input('Please enter your postal code in capital letters with a space inbetween. ')
@@ -50,14 +51,20 @@ def main():#'✓'
     
 
     
-    user = open('gmails.txt').read().splitlines()
+    #user = open('gmails.txt').read().splitlines()
+    
     ranfname = open('firstnames.txt', 'rt').read().splitlines()
     ranfname = [ranfname.replace(' ', '') for ranfname in ranfname]     
     
     ranlname = open('lastnames.txt', 'rt').read().splitlines()
     ranlname = [ranlname.replace(' ', '') for ranlname in ranlname]     
+    
     ranaddy = open('addyjug.txt', 'rt').read().splitlines()
+    #ranaddy = [ranaddy.replace(' ', '') for ranaddy in ranaddy]     
+
     ranaddy2 = open('addy2jug.txt', 'rt').read().splitlines()
+    #ranaddy2 = [ranaddy2.replace(' ', '') for ranaddy2 in ranaddy2]     
+
     mobilestart = '+44'
     mobilehalf = '7813' 
     
@@ -70,19 +77,20 @@ def main():#'✓'
         #randtime = random.randint(1,5) 
         proxy = get_proxy()
         session.cookies.clear()
-        user2 = random.choice(user)
-        domainfinal = user2 + '+' + str(random.randint(1,10000)) + '@gmail.com'
+        user2 = random.choice(ranfname)
+        domainfinal = user2 + str(random.randint(1,10000)) + catchall
         randletter = random.choice(str(string.ascii_uppercase[0:25]))
         randletter2 = random.choice(str(string.ascii_uppercase[0:25]))
         randletter3 = random.choice(str(string.ascii_uppercase[0:25]))
         ranaddy2finish = random.choice(ranaddy2)
         ranaddyfinish = random.choice(ranaddy)
+        ranaddyfinish = ranaddyfinish[:9] + randletter.lower() + ranaddyfinish[9:]
         mobileend = str(random.randint(100000,999999))
-        ranfname2 = random.choice(ranfname)
+        ranfname2 = user2
         ranlname2 = random.choice(ranlname)
         mobilenotfinish = mobilehalf + mobileend
         mobilefinish =   mobilestart + mobilehalf + mobileend
-        addyfinish = randletter+randletter2+randletter3+ " " + ranaddyfinish
+        addyfinish = ranaddyfinish + randletter2.lower()
 
         load = {
                 "comp_firstname":ranfname2,
@@ -114,6 +122,7 @@ def main():#'✓'
                            }
         
         post = session.post(url,data=load)
+        #print (len(post.content))
         info = domainfinal + ": " + addyfinish + ": " + str(shoesize) + ": " + ranfname2 + ": " + ranlname2 + ": " + mobilefinish
         
         if "https://reporting.jdsports.co.uk/images/yeezy/thankyou.jpg"  in post.text:
